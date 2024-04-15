fa8fcc03b2a64451e6b8669c4f7653e71880c1b8
mushroomvoodoo <normeeboy@gmail.com>
2024-04-15 19:45:18 +0300

    ..

--- LabaWS6/Program.cs
+++ LabaWS6/Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿using LabaWS6;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        Console.WriteLine(new RealShip().GetAllInfo());
+        Console.WriteLine(new RealPlane().GetAllInfo());
+        Console.WriteLine(new RealAuto().GetAllInfo());
     }
 }
--- LabaWS6/Transport.cs
+++ LabaWS6/Transport.cs
@@ -18,36 +18,53 @@ namespace LabaWS6
     }
     abstract class Automobile : Transport
     {
-        protected override double Speed { set { Speed = 300; } }
-        protected override double LiftCapacity { set => LiftCapacity = 200; }
-        protected override double DistanceRange { set => DistanceRange = 1000; }
-        override public double GetSpeed() { return this.Speed; }
-        public override double GetLiftCapacity() { return this.LiftCapacity; }
-        public override double GetDistanceRange() { return this.DistanceRange; }
+        protected string MarkModel { set; get; } = "Лада Веста";
+        protected override double Speed { set; get; } = 150;
+        protected override double LiftCapacity { get; set; } = 100;
+        protected override double DistanceRange { set; get; } = 450;
+        override public double GetSpeed() { return Speed; }
+        public override double GetLiftCapacity() { return LiftCapacity; }
+        public override double GetDistanceRange() { return DistanceRange; }
     }
     abstract class Airplane : Transport
     {
-        protected override double Speed { set { Speed = 111; } }
-        protected override double LiftCapacity { set => LiftCapacity = 200; }
-        protected override double DistanceRange { set => DistanceRange = 1000000; }
-        override public double GetSpeed() { return this.Speed; }
-        public override double GetLiftCapacity() { return this.LiftCapacity; }
-        public override double GetDistanceRange() { return this.DistanceRange; }
+        protected string Model { set; get; } = "ИЛ-52";
+        protected override double Speed { set; get; } = 200;
+        protected override double LiftCapacity { get; set; } = 1500;
+        protected override double DistanceRange { set; get; } = 25000;
+        override public double GetSpeed() { return Speed; }
+        public override double GetLiftCapacity() { return LiftCapacity; }
+        public override double GetDistanceRange() { return DistanceRange; }
     }
     abstract class Ship : Transport
     {
-        protected override double Speed { set { Speed = 111; } }
-        protected override double LiftCapacity { set => LiftCapacity = 1500; }
-        protected override double DistanceRange { set => DistanceRange = 100000000; }
+        protected string Name { set; get; } = "Непотопляемый";
+        protected override double Speed { set; get; } = 100;
+        protected override double LiftCapacity { get; set; } = 1500000;
+        protected override double DistanceRange { set; get; } = 10000000;
         override public double GetSpeed() { return this.Speed; }
         public override double GetLiftCapacity() { return this.LiftCapacity; }
         public override double GetDistanceRange() { return this.DistanceRange; }
     }
     class RealShip : Ship
     {
-        protected override double Speed { set { Speed = 111; } get { return this.Speed; }
-}
-        protected override double LiftCapacity { set => LiftCapacity = 1500; }
-        protected override double DistanceRange { set => DistanceRange = 100000000; }
+        public string GetAllInfo()
+        {
+            return $"Корабль \"{Name}\" со скоростью {GetSpeed()} км/ч, с грузоподъемностью {GetLiftCapacity()} кг и с дальностью расстояния {GetDistanceRange()} км";
+        }
+    }
+    class RealPlane : Airplane
+    {
+        public string GetAllInfo()
+        {
+            return $"Самолет \"{Model}\" со скоростью {GetSpeed()} км/ч, с грузоподъемностью {GetLiftCapacity()} кг и с дальностью расстояния {GetDistanceRange()} км";
+        }
+    }
+    class RealAuto : Automobile
+    {
+        public string GetAllInfo()
+        {
+            return $"Автомобиль \"{MarkModel}\" со скоростью {GetSpeed()} км/ч, с грузоподъемностью {GetLiftCapacity()} кг и с дальностью расстояния {GetDistanceRange()} км";
+        }
     }
 }
